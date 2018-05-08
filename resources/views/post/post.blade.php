3dc153e01e5cd4a4987c35b4a056ed5c17a09948
Rehan Mazhar <rehansyed89@gmail.com>
2018-05-08 21:51:12 +0200

    Project initialization

---
+++ resources/views/post/post.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.app')
+
+@section('main')
+    Article page
+@endsection
---
+++ resources/views/templates/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700,800" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('main')
+        </div>
+    </body>
+</html>
